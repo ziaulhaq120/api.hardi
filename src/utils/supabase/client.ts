8f9e2b707d3f2c4cae56d45f14c27c666540f72c
ziaulhaq <ziaaajr@gmail.com>
2025-01-21 00:44:04 +0700

    Delete src/utils/supabase directory

--- src/utils/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js'
-
-export const createClient = () => {
-  return createSupabaseClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
-  )
-} 
